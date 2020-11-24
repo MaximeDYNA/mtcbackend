3605a0ab8c048e866d0027bfb5ac97c204f17898
chefban <franckytchoko@gmail.com>
2020-11-24 16:40:14 +0100

    mis à jour adresse

--- src/main/java/com/catis/model/Adresse.java
+++ src/main/java/com/catis/model/Adresse.java
@@ -1,11 +1,12 @@
 package com.catis.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
