bd1acf44ccc9283c5ebfd19afa97d36eae45c727
chefban <franckytchoko@gmail.com>
2021-01-27 13:06:52 +0100

    correction signature

---
+++ src/main/java/com/catis/repository/CategorieTestVehicule.java
@@ -0,0 +1,9 @@
+package com.catis.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.catis.model.CategorieTest;
+
+public interface CategorieTestVehicule extends CrudRepository<CategorieTest, Long>{
+
+}
