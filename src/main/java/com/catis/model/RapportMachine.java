ce6694caab5dbc8a83c1957add11c4cd80d9fbac
Aubry Yvan <yvanovichaubry69@gmail.com>
2021-02-02 12:48:49 +0100

    update database schema

---
+++ src/main/java/com/catis/model/RapportMachine.java
@@ -0,0 +1,24 @@
+package com.catis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RapportMachine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "rapportMachine")
+    private Set<CategorieTestMachine> categorieTestMachines;
+}
