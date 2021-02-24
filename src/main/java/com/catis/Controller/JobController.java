8c6e68d212a6a0ac1ea5fc249e0cdd31d1f409b3
chefban <franckytchoko@gmail.com>
2021-02-24 15:50:00 +0100

    corrections pour nouveau serveur

---
+++ src/main/java/com/catis/Controller/JobController.java
@@ -0,0 +1,36 @@
+package com.catis.Controller;
+
+import com.catis.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobController {
+
+    @Autowired
+    private VisiteService vs;
+    @Autowired
+    private ProduitService ps;
+    @Autowired
+    private GieglanFileService gieglanFileService;
+    @Autowired
+    private CategorieTestVehiculeService catSer;
+
+    @GetMapping("/api/v1/visite/maj/{id}")
+    public void majvisiteEvent(@PathVariable Long id){
+        try {
+
+            VisiteController.dispatchEdit(vs.findById(id),
+                    vs, gieglanFileService, catSer, ps);
+            System.out.println("le Job a effectué un chanqement sur la visite n°"+id+" :)");
+
+        }
+        catch (Exception e) {
+            System.out.println("Erreur survenur lors de la notification du Job");
+        }
+    }
+}
