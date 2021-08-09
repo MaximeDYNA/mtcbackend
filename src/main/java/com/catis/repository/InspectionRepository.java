4d0e399e37670bb1bbda888c66bf59abd5e0e4fb
chefban <franckytchoko@gmail.com>
2021-08-09 15:16:40 +0200

    only active inspection are visible on tab

--- src/main/java/com/catis/repository/InspectionRepository.java
+++ src/main/java/com/catis/repository/InspectionRepository.java
@@ -14,7 +14,7 @@ public interface InspectionRepository extends CrudRepository<Inspection, Long> {
     List<Inspection> findByVisite_StatutAndLigne_IdLigne(int statut, Long idLigne);
 
     @Query("select i from Inspection i inner join i.visite v " +
-            "where i.ligne.idLigne = ?2 and v.statut = ?1 and i.visibleToTab = true " +
+            "where i.ligne.idLigne = ?2 and v.statut = ?1 and i.visibleToTab = true and i.activeStatus = true " +
             "group by i.idInspection")
     List<Inspection> inspectionbyligneAndVisibleToTabTrue(int statut, Long idLigne);
 
