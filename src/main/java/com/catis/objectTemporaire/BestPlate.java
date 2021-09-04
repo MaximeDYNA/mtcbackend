07e8edf0db18b3beda808286125951987b313b2c
chefban <franckytchoko@gmail.com>
2021-09-04 15:39:46 +0200

    optimize time to have visit list template adjustement

---
+++ src/main/java/com/catis/objectTemporaire/BestPlate.java
@@ -0,0 +1,11 @@
+package com.catis.objectTemporaire;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BestPlate {
+    private String plate;
+    private double rate;
+}
