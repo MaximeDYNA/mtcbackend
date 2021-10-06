5f58e6ec0974f2c905fd8b694024e2884d7fbae2
chefban <franckytchoko@gmail.com>
2021-10-06 18:13:07 +0200

    reduce memory usage

--- src/main/java/com/catis/repository/VisiteRepository.java
+++ src/main/java/com/catis/repository/VisiteRepository.java
@@ -47,8 +47,10 @@ public interface VisiteRepository extends CrudRepository<Visite, Long> {
 
     Page<Visite> findByOrganisation_OrganisationIdAndEncoursFalseAndActiveStatusTrueOrderByCreatedDateDesc(Long orgId, Pageable pageable);
 
-    @Query("select v from Visite v join fetch v.caissier c join fetch c.partenaire p " +
-            "join fetch c.proprietaireVehicule where " +
+    @Query("select v from Visite v join fetch v.caissier c " +
+            "join fetch c.partenaire p " +
+            "join fetch v.carteGrise ca " +
+            "join fetch ca.proprietaireVehicule where " +
             "(:ref is null or (lower(v.caissier.partenaire.nom) like lower(concat('%', :ref,'%')))" +
             "or (lower(v.carteGrise.proprietaireVehicule.partenaire.nom) like lower(concat('%', :ref,'%'))) " +
             "or (lower(v.carteGrise.numImmatriculation) like lower(concat('%', :ref,'%')))) " +
