c11fb1ce4a23e4248ce6f522ad98aa018d74ebcf
chefban <franckytchoko@gmail.com>
2021-12-06 15:05:30 +0100

    edit sse

--- src/main/java/com/catis/service/EmitterService.java
+++ src/main/java/com/catis/service/EmitterService.java
@@ -7,31 +7,30 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
-//@Slf4j
+@Slf4j
 public class EmitterService {
 
     private final long eventsTimeout;
     private final EmitterRepository repository;
 
-    public EmitterService(@Value("180000") long eventsTimeout,
+    public EmitterService(@Value("${events.connection.timeout}") long eventsTimeout,
                           EmitterRepository repository) {
         this.eventsTimeout = Long.MAX_VALUE;
         this.repository = repository;
     }
 
-    public SseEmitter createEmitter(String memberId) {
-        //log.info("Create SseEmitter for {}", memberId);
-        SseEmitter emitter = new SseEmitter(eventsTimeout);
-        repository.addOrReplaceEmitter(memberId, emitter);
 
+    public SseEmitter createEmitter(String memberId) {
+        SseEmitter emitter = new SseEmitter(eventsTimeout);
         emitter.onCompletion(() -> repository.remove(memberId));
         emitter.onTimeout(() -> repository.remove(memberId));
         emitter.onError(e -> {
-        //    log.error("Create SseEmitter exception", e);
+            log.error("Create SseEmitter exception", e);
             repository.remove(memberId);
         });
-
+        repository.addOrReplaceEmitter(memberId, emitter);
         return emitter;
     }
+
 
 }
